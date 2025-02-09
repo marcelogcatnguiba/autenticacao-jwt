83eef29bf21076afc951ba9fc1408ac18b2d25dc
Marcelo <marcelogomes.0104@gmail.com>
2025-02-09 11:57:33 -0300

    refactor: add debug

--- src/AuthSample.Api.Controller/Controllers/HomeController.cs
+++ src/AuthSample.Api.Controller/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ namespace AuthSample.Api.Controller.Controllers;
 [Authorize(Roles = Usuario.Administrador)]
 [ApiController]
 [Route("[controller]")]
+[ProducesResponseType<WelcomeMessage>(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status401Unauthorized)]
 public class HomeController : ControllerBase
 {
     [HttpGet]
     public IActionResult Welcome()
     {
-        return Ok(new { message = "Bem vindo" });
+        return Ok(new WelcomeMessage("Bem vindo"));
     }
 }
+
+public record WelcomeMessage(string Message);
